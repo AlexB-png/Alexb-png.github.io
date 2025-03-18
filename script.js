ecfc2d0c2f4ab748d2ce602c0ac23242c5a3f4c2
alexb-png <alexander.brankin.466@accesscreative.ac.uk>
2025-03-18 20:35:38 +0000

    works

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const title = document.querySelector("h1");
-// title.innerHTML = "hehe sd31";
-
-// setInterval(() => {
-//   title.innerHTML = title.innerHTML += "1";
-// }, 1000);
-
-// makes a window appear that says hello with opt to close
-// const button = document.querySelector("button");
-// button.addEventListener("click", () => {
-//   dialog.close()
-// })
-
-// const dialog = document.querySelector("#MyDialog");
-// setTimeout(() => {
-//   dialog.showModal();
-// }, 5000);
-
 const PythonDialog = document.querySelector("#python");
 
 const python = document.querySelector("#PythonButton");
